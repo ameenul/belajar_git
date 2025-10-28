f8ff89bd24a670dfcf1a4fa6e85f607c9eb2ead9
akbar <ameenul@gmail.com>
2025-10-28 10:05:26 +0700

    coba beda email

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Welcome to the Java Application.");
+        System.out.println("cek beda email");
     }
 }
